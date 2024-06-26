395f94de76578a5ed360b4efa9b626475be1132e
Max Siz <maxsizmobile@gmail.com>
2024-06-26 19:01:36 +0700

    FEAT impl csceleton

---
+++ src/impl/wnftLegacy.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+/**
+ * The WnftLegacy contract does this and that...
+ */
+contract WnftLegacy {
+  constructor() public {
+    
+  }
+}
