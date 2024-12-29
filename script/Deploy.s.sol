614f8b33f2539a32bed34a85bc61558c919f00ba
Max Siz <maxsizmobile@gmail.com>
2024-12-29 17:41:51 +0700

    FEAT new  deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -12,6 +12,7 @@ import "../src/impl/WNFTLegacy721.sol";
 import "../src/impl/WNFTV2Envelop721.sol";
 import "../src/impl/WNFTMyshchWallet.sol";
 import "../src/impl/WNFTV2Index.sol";
+import "../src/impl/CustomERC20.sol";
 import {EnvelopLegacyWrapperBaseV2} from "../src/EnvelopLegacyWrapperBaseV2.sol";
 
 
@@ -26,11 +27,8 @@ import {EnvelopLegacyWrapperBaseV2} from "../src/EnvelopLegacyWrapperBaseV2.sol"
 
 // Address:     0x6F9aaAaD96180b3D6c71Fbbae2C1c5d5193A64EC
 // Private key: 0xae8fe3985898986377b19cc6bdbb76723470552e95e4d028d2dae2691ab9c65d
-
-
-contract DeployScript is Script {
+abstract contract Objects is Script{
     using stdJson for string;
-
     struct Params {
         address factory;   
         address legacy_wrapper;
@@ -38,22 +36,27 @@ contract DeployScript is Script {
         address impl_native;
         address impl_myshch;
         address impl_index;
+        address impl_erc20;
         bool need_test_tx;
+        address[] erc20mock;
     }
 
     Params p;
-    address[] implementations;  
+    address[] implementations; 
 
-    function run() public {
-        console2.log("Chain id: %s", vm.toString(block.chainid));
-        console2.log(
-            "Deployer address: %s, "
-            "\n native balnce %s",
-            msg.sender, msg.sender.balance
-        );
-         
+    string public params_json_file = vm.readFile(string.concat(vm.projectRoot(), "/script/chain_params.json"));
+
+    EnvelopWNFTFactory factory;
+    EnvelopLegacyWrapperBaseV2 wrapper;
+    WNFTLegacy721 impl_legacy;
+    WNFTV2Envelop721 impl_native;
+    WNFTMyshchWallet impl_myshch;
+    WNFTV2Index impl_index; 
+    CustomERC20 impl_erc20;
+
+    function getChainParams() internal {
         // Load json with chain params
-        string memory params_json_file = vm.readFile(string.concat(vm.projectRoot(), "/script/chain_params.json"));
+        
         string memory key;
         
         // Define constructor params
@@ -105,6 +108,25 @@ contract DeployScript is Script {
             p.impl_index = address(0);
         }
 
+
+        key = string.concat(".", vm.toString(block.chainid),".erc20");
+        if (vm.keyExists(params_json_file, key)) 
+        {
+            address[] memory aa = params_json_file.readAddressArray(key);
+            for (uint256 i = 0; i < aa.length; ++i ){
+                p.erc20mock.push(aa[i]);   
+                console2.log("erc20 mock: %s", aa[i]); 
+            }
+        }
+
+        key = string.concat(".", vm.toString(block.chainid),".impl_erc20");
+        if (vm.keyExists(params_json_file, key)) 
+        {
+            p.impl_erc20 = params_json_file.readAddress(key);
+        } else {
+            p.impl_erc20 = address(0);
+        }
+
         key = string.concat(".", vm.toString(block.chainid),".need_test_tx");
         if (vm.keyExists(params_json_file, key)) 
         {
@@ -112,83 +134,88 @@ contract DeployScript is Script {
         } else {
             p.need_test_tx = false;
         }
+    }
 
-
-        // key = string.concat(".", vm.toString(block.chainid),".neededERC20Amount");
-        // if (vm.keyExists(params_json_file, key)) 
-        // {
-        //     p.neededERC20Amount = params_json_file.readUint(key);
-        // } else {
-        //     p.neededERC20Amount = 0;
-        // }
-        // console2.log("neededERC20Amount: %s", p.neededERC20Amount); 
-        
-        
-        
-
-        //////////   Deploy   //////////////
-        vm.startBroadcast();
-        EnvelopWNFTFactory factory;
-        EnvelopLegacyWrapperBaseV2 wrapper;
-        WNFTLegacy721 impl_legacy;
-        WNFTV2Envelop721 impl_native;
-        WNFTMyshchWallet impl_myshch;
-        WNFTV2Index impl_index;
-
+    function deployOrInstances(bool onlyInstance) internal {
         //factory = EnvelopWNFTFactory(0x431Db5c6ce5D85A0BAa2198Aa7Aa0E65d37a25c8);
         if (p.factory == address(0)) {
-            factory = new EnvelopWNFTFactory();    
+            if (!onlyInstance) {
+                factory = new EnvelopWNFTFactory();    
+            }
         } else {
             factory = EnvelopWNFTFactory(p.factory);
         }
 
         if (p.legacy_wrapper == address(0)){
-            wrapper = new EnvelopLegacyWrapperBaseV2(address(factory)); 
-            factory.setWrapperStatus(address(wrapper), true); // set wrapper
+            if (!onlyInstance) {
+                wrapper = new EnvelopLegacyWrapperBaseV2(address(factory)); 
+                factory.setWrapperStatus(address(wrapper), true); // set wrapper
+            }
         } else {
             wrapper = EnvelopLegacyWrapperBaseV2(p.legacy_wrapper);    
         }
         implementations.push(address(wrapper));
         
         if (p.impl_legacy == address(0)) {
-            impl_legacy = new WNFTLegacy721();
-            wrapper.setWNFTId(
-                ET.AssetType.ERC721, 
-                address(impl_legacy), 
-                impl_legacy.TOKEN_ID()
-            );
+            if (!onlyInstance) {
+                impl_legacy = new WNFTLegacy721();
+                wrapper.setWNFTId(
+                    ET.AssetType.ERC721, 
+                    address(impl_legacy), 
+                    impl_legacy.TOKEN_ID()
+                );
+            }
         } else {
             impl_legacy = WNFTLegacy721(payable(p.impl_legacy));
         }
         
         if (p.impl_native == address(0)) {
-            impl_native = new WNFTV2Envelop721(address(factory)); 
-           // if (!factory.trustedWrappers(address(impl_native))) {
-                factory.setWrapperStatus(address(impl_native), true); // set wrapper    
-           // }   
-            
+            if (!onlyInstance) {
+                impl_native = new WNFTV2Envelop721(address(factory)); 
+               // if (!factory.trustedWrappers(address(impl_native))) {
+                    factory.setWrapperStatus(address(impl_native), true); // set wrapper    
+               // }   
+            }
         } else {
             impl_native = WNFTV2Envelop721(payable(p.impl_native));
         }
         implementations.push(address(impl_native));
 
         if (p.impl_myshch == address(0)) {
-            impl_myshch = new WNFTMyshchWallet(address(factory),0);    
-            //factory.setWrapperStatus(address(impl_myshch), true); // set wrapper
+            if (!onlyInstance) {
+                impl_myshch = new WNFTMyshchWallet(address(factory),0);    
+                //factory.setWrapperStatus(address(impl_myshch), true); // set wrapper
+            }
         } else {
             impl_myshch = WNFTMyshchWallet(payable(p.impl_myshch));
         }
         implementations.push(address(impl_myshch));
 
         if (p.impl_index == address(0)) {
-            impl_index = new WNFTV2Index(address(factory));    
-            //factory.setWrapperStatus(address(impl_myshch), true); // set wrapper
+            if (!onlyInstance) {
+                impl_index = new WNFTV2Index(address(factory));    
+                //factory.setWrapperStatus(address(impl_myshch), true); // set wrapper
+            }
         } else {
             impl_index = WNFTV2Index(payable(p.impl_index));
         }
         implementations.push(address(impl_index));
-        vm.stopBroadcast();
-        
+
+        // ERC20 moock
+        if (p.impl_erc20 == address(0)) {
+            if (!onlyInstance) {
+                impl_erc20 = new CustomERC20();    
+            }
+        } else {
+            impl_erc20 = CustomERC20(p.impl_erc20);
+        }
+
+
+        //vm.stopBroadcast();
+
+    }
+
+    function prettyPrint() internal {
         ///////// Pretty printing ////////////////
         
         //string memory path = string.concat(vm.projectRoot(), "/script/explorers.json");
@@ -213,6 +240,8 @@ contract DeployScript is Script {
         console2.log("https://%s/address/%s#code\n", explorer_url, address(impl_myshch));
         console2.log("\n**WNFTV2Index** ");
         console2.log("https://%s/address/%s#code\n", explorer_url, address(impl_index));
+        console2.log("\n**CustomERC20** ");
+        console2.log("https://%s/address/%s#code\n", explorer_url, address(impl_erc20));
 
 
 
@@ -223,7 +252,41 @@ contract DeployScript is Script {
         console2.log("impl_native = WNFTV2Envelop721.at('%s')", address(impl_native));
         console2.log("impl_myshch = WNFTMyshchWallet.at('%s')", address(impl_myshch));
         console2.log("impl_index = WNFTV2Index.at('%s')", address(impl_index));
+        console2.log("impl_erc20 = CustomERC20.at('%s')", address(impl_erc20));
         console2.log("```");
+
+        console2.log("```json");
+        console2.log("\"factory\": \"%s\",", address(factory));
+        console2.log("\"wrapper\": \"%s\",", address(wrapper));
+        console2.log("\"impl_legacy\": \"%s\",", address(impl_legacy));
+        console2.log("\"impl_native\": \"%s\",", address(impl_native));
+        console2.log("\"impl_myshch\": \"%s\",", address(impl_myshch));
+        console2.log("\"impl_index\": \"%s\",", address(impl_index));
+        console2.log("\"impl_erc20\": \"%s\"", address(impl_erc20));
+        console2.log("```");
+    }
+
+}
+
+contract DeployScript is Script, Objects {
+    using stdJson for string;
+    function run() public {
+        console2.log("Chain id: %s", vm.toString(block.chainid));
+        console2.log(
+            "Deployer address: %s, "
+            "\n native balnce %s",
+            msg.sender, msg.sender.balance
+        );
+         
+        getChainParams();
+
+        //////////   Deploy   //////////////
+        vm.startBroadcast();
+        deployOrInstances(false);
+        vm.stopBroadcast();
+
+        prettyPrint(); 
+        
    
         // ///////// End of pretty printing ////////////////
         
@@ -265,4 +328,127 @@ contract DeployScript is Script {
         vm.stopBroadcast();
         console2.log("Initialisation finished");
     }
+}
+
+contract TestTxScript is Script, Objects {
+    using stdJson for string;
+
+    //string params_json_file = vm.readFile(string.concat(vm.projectRoot(), "/script/explorers.json"));
+    
+    
+    function run() public {
+        console2.log("Chain id: %s", vm.toString(block.chainid));
+        console2.log(
+            "Deployer address: %s, "
+            "\n native balnce %s",
+            msg.sender, msg.sender.balance
+        );
+
+        getChainParams();
+        deployOrInstances(true);
+        vm.startBroadcast();
+        
+        // Create Smart wallet test 
+        WNFTV2Envelop721.InitParams memory initData = WNFTV2Envelop721.InitParams(
+            msg.sender,
+            'Envelop V2 Smart Wallet',
+            'ENVELOPV2',
+            'https://api.envelop.is/wallet',
+            new address[](0),
+            new bytes32[](0),
+            new uint256[](0),
+            ""
+        );
+
+        address payable _wnftWalletAddress = payable(impl_native.createWNFTonFactory(initData));
+
+        WNFTV2Index wnftWallet = WNFTV2Index(_wnftWalletAddress);
+        
+        // Topup Smart wallet test
+        for (uint256 i = 0; i < p.erc20mock.length; ++ i) {
+            IERC20(p.erc20mock[i]).transfer(_wnftWalletAddress, 1e18);
+            
+        }
+        _wnftWalletAddress.call{value: 3e14}("");
+
+        //
+        // make new wnftWallet using executeOp
+        // transfer erc20 tyokens to new wnftWallet
+        // make 1 child wallet
+        // transfer erc20 tokens from master wallet to child wallets
+        // transfer ether
+
+        // calc index wallet addresses
+        bytes32 salt = keccak256(abi.encode(address(impl_index), impl_index.nonce() + 1));
+        address calcW1 = factory.predictDeterministicAddress(address(impl_index), salt);
+
+        address[] memory targets = new address[](3);
+        bytes[] memory dataArray = new bytes[](3);
+        uint256[] memory values = new uint256[](3);
+
+        targets[0] = address(impl_index);
+        targets[1] = address(p.erc20mock[0]);
+        targets[2] = address(calcW1);
+
+        // prepare data for deploying of child wallets
+        initData = WNFTV2Envelop721.InitParams(
+            msg.sender,
+            '',
+            '',
+            '',
+            new address[](0),
+            new bytes32[](0),
+            new uint256[](0),
+            ""
+            );
+
+        // using method with salt
+        bytes memory _data = abi.encodeWithSignature(
+            "createWNFTonFactory2((address,string,string,string,address[],bytes32[],uint256[],bytes))",
+            initData
+        );
+
+       
+        values[0] = 0;    
+        values[1] = 0;    
+        values[2] = 16;            
+       
+
+        dataArray[0] = _data;
+        dataArray[1] = abi.encodeWithSignature(
+            "transfer(address,uint256)",
+            calcW1, 2e14
+        );
+       
+        dataArray[2] = "";
+
+        bytes[] memory result = wnftWallet.executeEncodedTxBatch(targets, values, dataArray);
+
+        // get child wallet adresses from output
+        address payable w1 =  payable(abi.decode(result[0],
+             (address)
+        ));
+
+        vm.stopBroadcast();
+
+        params_json_file = vm.readFile(string.concat(vm.projectRoot(), "/script/explorers.json"));
+        string memory explorer_url = params_json_file.readString(
+            string.concat(".", vm.toString(block.chainid))
+        );
+
+        console2.log(
+            "Deployed index contract:"
+            "\n https://%s/address/%s#code\n",
+            explorer_url, w1
+        );
+
+        console2.log(
+            "from Envelop Smart Wallet:"
+            "\n https://%s/address/%s#code\n",
+            explorer_url, _wnftWalletAddress
+        );
+
+    
+    }
+
 }
--- script/README.md
+++ script/README.md
@@ -2,12 +2,18 @@
 !!! For use cli below don't forget set appropriate variables in `.env` (see Development section) 
 ### Sepolia
 ```shell
-$ forge script script/Deploy.s.sol:DeployScript --rpc-url sepolia  --account three --sender 0x97ba7778dD9CE27bD4953c136F3B3b7b087E14c1 --broadcast --verify  --etherscan-api-key $ETHERSCAN_TOKEN --priority-gas-price 30000
+$ forge script script/Deploy.s.sol:DeployScript --rpc-url sepolia  --account three --sender 0x97ba7778dD9CE27bD4953c136F3B3b7b087E14c1 --broadcast --verify  --etherscan-api-key $ETHERSCAN_TOKEN 
+$ # Test Tx Script
+$ forge script script/Deploy.s.sol:TestTxScript --rpc-url sepolia  --account three --sender 0x97ba7778dD9CE27bD4953c136F3B3b7b087E14c1 --broadcast --verify  --etherscan-api-key $ETHERSCAN_TOKEN 
 
 $ forge script script/DeployMyshchSet.s.sol:DeployMyshchSetScript --rpc-url sepolia  --account three --sender 0x97ba7778dD9CE27bD4953c136F3B3b7b087E14c1 --broadcast --verify  --etherscan-api-key $ETHERSCAN_TOKEN --priority-gas-price 30000
 
 $ forge script script/DeployMyshchSet.s.sol:TestTxScript --rpc-url sepolia  --account three --sender 0x97ba7778dD9CE27bD4953c136F3B3b7b087E14c1 --broadcast --verify  --etherscan-api-key $ETHERSCAN_TOKEN --priority-gas-price 30000
 
+$ #######  Deploy ERC20 custom
+$ cast send 0x7F3f876463e3f70634823be78F6f55E01720A068 "createCustomERC20(address,string,string,uint256,(address,uint256)[])" "0x97ba7778dD9CE27bD4953c136F3B3b7b087E14c1" "Test NIFTSY TOKEN 2025" "NIFTSY" "1000000000000000000000000000" "[]" --rpc-url sepolia  --account three 
+
+$ cast send 0x7F3f876463e3f70634823be78F6f55E01720A068 "createCustomERC20(address,string,string,uint256,(address,uint256)[])" "0x97ba7778dD9CE27bD4953c136F3B3b7b087E14c1" "Test DAI Token" "NIFTSY" "2000000000000000000000000000" "[]" --rpc-url sepolia  --account three 
 
 ```
 
