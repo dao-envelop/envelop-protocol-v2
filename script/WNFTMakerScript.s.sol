75569646bae08796dbd40fa3ec74bdcc0d2be780
Alex <alexamuek@gmail.com>
2025-01-29 16:44:54 +0800

    update script fro defi

--- script/WNFTMakerScript.s.sol
+++ script/WNFTMakerScript.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.21;
 import "./Objects.s.sol";
 import {Script, console2} from "forge-std/Script.sol";
 import "../lib/forge-std/src/StdJson.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
 
@@ -75,12 +76,12 @@ contract WNFTMakerScript is Script, Objects {
         // prepare batch of transactions
 
         // 0. make approve for router
-        /*address target = usdt_address;
+        address target = usdt_address;
         bytes memory _data = abi.encodeWithSignature(
             "approve(address,uint256)",
             router,amount_to_swap
         );
-        uint256 value = 0;*/
+        uint256 value = 0;
 
         // 1. create child wallets (indexes)
 
@@ -104,26 +105,26 @@ contract WNFTMakerScript is Script, Objects {
 
         dataArray[0] = _dataIndex;
         dataArray[1] = _dataIndex;
-        dataArray[2] = "0x83bd37f9000155d398326f99059ff775485246999027b319795500018ac76a51cc950d9822d68b83fe1ad97b32cd580d072386f26fc100000723864e50a1965000c49b0001C46785891bc0dc3A1b88a6ba39A78aB7f5850846000000018d1454F9ac6363e20664C1AE29bF47C38a354f250000000003010203000e0101010201ff0000000000000000000000000000000000000000001b9a1120a17617d8ec4dc80b921a9a1c50caef7d55d398326f99059ff775485246999027b3197955000000000000000000000000000000000000000000000000";
-        
+        dataArray[2] = hex"84a7f3dd01020001C46785891bc0dc3A1b88a6ba39A78aB7f58508460b097854efd41c1580000000000155d398326f99059ff775485246999027b3197955072386f26fc10000000000018ac76a51cc950d9822d68b83fe1ad97b32cd580d043b98a0c900018d1454F9ac6363e20664C1AE29bF47C38a354f250000059ddceea5c400018d1454F9ac6363e20664C1AE29bF47C38a354f2500000000060203060187d79f49030102010203001e000e0200040301040600000203020100010501140001ff000000000000000000000000000000000000000000000000006190e79064213e6a2997355153f57904fb4910c3764aecc7c8136f4501f75b3187ef6f8246c976ed55d398326f99059ff775485246999027b319795547a90a2d92a8367a91efa1906bfc8c1e05bf10c4431e0cd023a32532bf3969cddfc002c00e98429d00000000000000000000000000000000000000000000000000000000";
+    
 
         // calc child wallet addresses
-        /*bytes32 salt = keccak256(abi.encode(address(impl_index), impl_index.nonce() + 1));
+        bytes32 salt = keccak256(abi.encode(address(impl_index), impl_index.nonce() + 1));
         address calcW1 = factory.predictDeterministicAddress(address(impl_index), salt);
         salt = keccak256(abi.encode(address(impl_index), impl_index.nonce() + 2));
-        address calcW2 = factory.predictDeterministicAddress(address(impl_index), salt);*/
+        address calcW2 = factory.predictDeterministicAddress(address(impl_index), salt);
 
-        vm.startBroadcast();
-        // make approve 
+        /*vm.startBroadcast();
+        // 0. make approve 
         //bytes memory result = master.executeEncodedTx(target, value, _data);
 
-        // call transaction batch - with swap
+        // 1. call transaction batch - with swap
         // from 599 address
         bytes[] memory result = master.executeEncodedTxBatch(targets, values, dataArray);
-        vm.stopBroadcast();
+        vm.stopBroadcast();*/
 
         // get child wallet adresses from output
-        /*address payable w1 =  payable(abi.decode(result[0],
+        address payable w1 =  payable(abi.decode(result[0],
              (address)
         ));
 
@@ -132,6 +133,43 @@ contract WNFTMakerScript is Script, Objects {
         ));
 
         console2.log('child wallet 1 = ', w1);
-        console2.log('child wallet 2 = ', w2);*/
+        console2.log('child wallet 2 = ', w2);
+
+        // 2. transfer assets from master to indexes
+        address payable index_address1 = payable(0x61d9aFFa2f76fa83Fd8cA890Cc00Ee6c286bD502);
+        address payable index_address2 = payable(0x153A2c68FB748Ca01b99E5146e81443aa1dEE295);
+        address[] memory targets1 = new address[](4);
+        bytes[] memory dataArray1 = new bytes[](4);
+        uint256[] memory values1 = new uint256[](4);
+        targets1[0] = usdc_address;
+        targets1[1] = usdc_address;
+        targets1[2] = index_address1;
+        targets1[3] = index_address2;
+
+        dataArray1[0] = abi.encodeWithSignature(
+            "transfer(address,uint256)",
+            index_address1,IERC20(usdc_address).balanceOf(master_address)/2
+        );
+        dataArray1[1] = abi.encodeWithSignature(
+            "transfer(address,uint256)",
+            index_address2,IERC20(usdc_address).balanceOf(master_address)/2
+        );
+        dataArray1[2] = "";
+        dataArray1[3] = "";
+
+        values1[0] = 0;
+        values1[1] = 0;
+        values1[2] = address(master).balance / 2;
+        values1[3] = address(master).balance / 2;
+
+        /*vm.startBroadcast();
+        // 2. call transaction batch - transfer assets
+        // from 599 address
+        bytes[] memory result = master.executeEncodedTxBatch(targets1, values1, dataArray1);
+        vm.stopBroadcast();*/
+
+
+
+
     }
 }
