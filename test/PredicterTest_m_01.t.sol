abd2dc00a4b0d2f894fad1110313b7742c4f505e
Alex <alexamuek@gmail.com>
2026-01-09 12:11:33 +0800

    update tests

--- test/PredicterTest_m_01.t.sol
+++ test/PredicterTest_m_01.t.sol
@@ -3,42 +3,11 @@ pragma solidity ^0.8.24;
 
 import "forge-std/Test.sol";
 import "../src/utils/Predicter.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../src/utils/PredictionBuilder.sol";
+import "../src/mock/MockOracle.sol";
+import "../src/mock/MockERC20.sol";
 
-/// @dev Simple ERC20 mock for staking in tests.
-contract MockERC20 is ERC20 {
-    constructor() ERC20("Mock", "MOCK") {
-        _mint(msg.sender, 1e27);
-    }
-
-    function mint(address to, uint256 amount) external {
-        _mint(to, amount);
-    }
-}
-
-/// @dev Mock oracle returning a configurable price.
-contract MockOracle is IEnvelopOracle {
-    uint256 public price;
-
-    function setPrice(uint256 _price) external {
-        price = _price;
-    }
-
-    function getIndexPrice(address) external pure override returns (uint256) {
-        return 0;
-    }
-
-    function getIndexPrice(CompactAsset[] calldata)
-        external
-        view
-        override
-        returns (uint256)
-    {
-        return price;
-    }
-}
-
-contract PredicterTest_m_01 is Test {
+contract PredicterTest_m_01 is Test, PredictionBuilder {
     MockERC20 internal token;
     MockOracle internal oracle;
     Predicter internal predicter;
@@ -59,7 +28,7 @@ contract PredicterTest_m_01 is Test {
     LogFields l;
 
     function setUp() public {
-        token = new MockERC20();
+        token = new MockERC20("Mock", "MOCK");
         oracle = new MockOracle();
 
         predicter = new Predicter(feeBeneficiary, address(oracle));
@@ -69,143 +38,11 @@ contract PredicterTest_m_01 is Test {
         token.mint(userNo, 1_000 ether);
     }
 
-    // ------------------------------------------------------------
-    // Helpers
-    // ------------------------------------------------------------
-
-    function _buildPrediction(uint40 expiration, uint96 strikeAmount, uint96 predictedAmount)
-        internal
-        view
-        returns (Predicter.Prediction memory pred)
-    {
-        // One-asset portfolio
-        CompactAsset[] memory portfolio = new CompactAsset[](1);
-        uint96 portfolioAmount = 1e18;
-        portfolio[0] = CompactAsset({token: address(token), amount: portfolioAmount});
-
-        pred.strike = CompactAsset({token: address(token), amount: strikeAmount});
-        pred.predictedPrice = CompactAsset({token: address(token), amount: predictedAmount});
-        pred.expirationTime = expiration;
-        pred.resolvedPrice = 0;
-        pred.portfolio = portfolio;
-    }
-
-    // ------------------------------------------------------------
-    // _resolvePrediction via claim
-    // ------------------------------------------------------------
-
-    function test_resolvePrediction_setsResolvedPriceAndClaimPays() public {
+    function test_resolvePrediction_onlyWinnersBets() public {
         uint40 exp = uint40(block.timestamp + 1 days);
         uint96 strikeAmount = 1_000_000;
         uint96 predictedPrice = 100;
-        Predicter.Prediction memory pred = _buildPrediction(exp, strikeAmount, predictedPrice);
-
-        vm.prank(creator);
-        predicter.createPrediction(pred);
-
-        uint256 totalYesAmount;
-        uint256 totalNoAmount;
-        uint256 yesNum = 4111;
-        uint256 noNum = 3;
-        
-        address user;
-        // usersYes vote
-        for (uint256 i = 0; i < yesNum; i++) {
-            user = address(uint160(i+100));
-            token.mint(user, strikeAmount);
-            vm.startPrank(user);
-            token.approve(address(predicter), strikeAmount);
-            predicter.vote(creator, true);
-            //console2.log('user = ', user);
-            //console2.log('user balance = ', predicter.balanceOf(user, 1002111867590296475548309107748372481));
-            vm.stopPrank();
-            totalYesAmount += strikeAmount;
-            usersYes.push(user);
-        }
-
-        // usersNo vote 
-        for (uint256 i = yesNum; i < yesNum + noNum; i++) {
-            user = address(uint160(i));
-            vm.startPrank(user);
-            token.mint(user, strikeAmount);
-            token.approve(address(predicter), strikeAmount);
-            predicter.vote(creator, false);
-            totalNoAmount += strikeAmount;
-            usersNo.push(user);
-            vm.stopPrank();
-        }
-
-
-        l.estTotalFeeBenef = totalNoAmount * predicter.FEE_PROTOCOL_PERCENT() * predicter.SCALE()
-          /predicter.PERCENT_DENOMINATOR()/ predicter.SCALE();
-        console2.log('Estimate Beneficiary Fee Amount: %s', l.estTotalFeeBenef);    
-
-        l.estTotalFeeCreator = totalNoAmount * predicter.FEE_CREATOR_PERCENT() * predicter.SCALE()
-          /predicter.PERCENT_DENOMINATOR() / predicter.SCALE();
-        console2.log('Estimate Creator     Fee Amount: %s', l.estTotalFeeCreator);
-
-
-        l.oneYesReward = (totalNoAmount / yesNum - l.estTotalFeeBenef / yesNum- l.estTotalFeeCreator/ yesNum) ;
-        console2.log('Estimate Reward Amount: %s', l.oneYesReward);  
-        
-        // set oracle price > predictedPrice => predictedTrue = true (yes wins)
-        uint256 oraclePrice = 200;
-        oracle.setPrice(oraclePrice);
-
-        // jump after expiration
-        vm.warp(exp + 1);
-        
-        console2.log('predicter balance before all claimes: %s', token.balanceOf(address(predicter)));  
-        assertEq(token.balanceOf(address(predicter)), totalNoAmount + totalYesAmount);   
-        (uint256 yesToken, uint256 noToken) = predicter.hlpGet6909Ids(address(creator));
-        assertEq(
-            token.balanceOf(address(predicter)), 
-            predicter.totalSupply(yesToken) + predicter.totalSupply(noToken)
-        );
-
-        // claims
-        for (uint256 i = 0; i < usersYes.length; i++) {
-            vm.prank(usersYes[i]);
-            predicter.claim(creator);
-        }
-
-        // resolvedPrice should be set
-        (, , , uint96 resolvedPrice) = predicter.predictions(creator);
-        assertEq(resolvedPrice, oraclePrice);
-        //assertEq(token.balanceOf(address(predicter)), 0);
-        console2.log('predicter balance after all claimes:  %s', token.balanceOf(address(predicter)));
-        console2.log('creater   balance after all claimes:  %s', token.balanceOf(address(creator)));
-        console2.log('beneficia balance after all claimes:  %s', token.balanceOf(address(feeBeneficiary)));
-        console2.log('One Yes voter balance after claimes:  %s', token.balanceOf(usersYes[0]));
-
-        
-
-        // resolvedPrice should be set
-        /*(, , , uint96 resolvedPrice) = predicter.predictions(creator);
-        assertEq(resolvedPrice, 200);
-
-        uint256 balanceAfterUser   = token.balanceOf(userYes);
-        uint256 balanceAfterCr     = token.balanceOf(creator);
-        uint256 balanceAfterProto  = token.balanceOf(feeBeneficiary);
-        uint256 contractBalanceAfter = token.balanceOf(address(predicter));
-
-        // User must have received back stake + net reward > 0
-        assertGt(balanceAfterUser, balanceBeforeUser);
-
-        // Creator and protocol both get some fee cut
-        assertGt(balanceAfterCr, balanceBeforeCr);
-        assertGt(balanceAfterProto, balanceBeforeProto);
-
-        // Контракт уменьшил баланс (выплаты сделаны)
-        assertLt(contractBalanceAfter, contractBalanceBefore);*/
-    }
-
-
-function test_resolvePrediction_onlyWinnersBets() public {
-        uint40 exp = uint40(block.timestamp + 1 days);
-        uint96 strikeAmount = 1_000_000;
-        uint96 predictedPrice = 100;
-        Predicter.Prediction memory pred = _buildPrediction(exp, strikeAmount, predictedPrice);
+        Predicter.Prediction memory pred = _buildPrediction(address(token), exp, strikeAmount, predictedPrice);
 
         vm.prank(creator);
         predicter.createPrediction(pred);
@@ -227,19 +64,6 @@ function test_resolvePrediction_onlyWinnersBets() public {
             totalYesAmount += strikeAmount;
             usersYes.push(user);
         }
-
-        // usersNo vote 
-        for (uint256 i = yesNum; i < yesNum + noNum; i++) {
-            user = address(uint160(i));
-            vm.startPrank(user);
-            token.mint(user, strikeAmount);
-            token.approve(address(predicter), strikeAmount);
-            predicter.vote(creator, false);
-            totalNoAmount += strikeAmount;
-            usersNo.push(user);
-            vm.stopPrank();
-        }
-
 
         l.estTotalFeeBenef = totalNoAmount * predicter.FEE_PROTOCOL_PERCENT() * predicter.SCALE()
           /predicter.PERCENT_DENOMINATOR()/ predicter.SCALE();
@@ -276,6 +100,7 @@ function test_resolvePrediction_onlyWinnersBets() public {
         for (uint256 i = 0; i < usersYes.length; i++) {
             vm.prank(usersYes[i]);
             predicter.claim(creator);
+            assertEq(token.balanceOf(address(usersYes[i])), strikeAmount); 
         }
 
         // resolvedPrice should be set
@@ -294,7 +119,7 @@ function test_resolvePrediction_onlyWinnersBets() public {
         uint40 exp = uint40(block.timestamp + 1 days);
         uint96 strikeAmount = 1_000_000;
         uint96 predictedPrice = 100;
-        Predicter.Prediction memory pred = _buildPrediction(exp, strikeAmount, predictedPrice);
+        Predicter.Prediction memory pred = _buildPrediction(address(token), exp, strikeAmount, predictedPrice);
 
         vm.prank(creator);
         predicter.createPrediction(pred);
@@ -305,21 +130,10 @@ function test_resolvePrediction_onlyWinnersBets() public {
         uint256 noNum = 23;
         
         address user;
-        // usersYes vote
-        for (uint256 i = 0; i < yesNum; i++) {
-            user = address(uint160(i+100));
-            token.mint(user, strikeAmount);
-            vm.startPrank(user);
-            token.approve(address(predicter), strikeAmount);
-            predicter.vote(creator, true);
-            vm.stopPrank();
-            totalYesAmount += strikeAmount;
-            usersYes.push(user);
-        }
 
         // usersNo vote 
         for (uint256 i = yesNum; i < yesNum + noNum; i++) {
-            user = address(uint160(i=100));
+            user = address(uint160(i + 100));
             vm.startPrank(user);
             token.mint(user, strikeAmount);
             token.approve(address(predicter), strikeAmount);
@@ -329,7 +143,6 @@ function test_resolvePrediction_onlyWinnersBets() public {
             vm.stopPrank();
         }
 
-
         l.estTotalFeeBenef = totalNoAmount * predicter.FEE_PROTOCOL_PERCENT() * predicter.SCALE()
           /predicter.PERCENT_DENOMINATOR()/ predicter.SCALE();
         console2.log('Estimate Beneficiary Fee Amount: %s', l.estTotalFeeBenef);    
@@ -338,9 +151,6 @@ function test_resolvePrediction_onlyWinnersBets() public {
           /predicter.PERCENT_DENOMINATOR() / predicter.SCALE();
         console2.log('Estimate Creator     Fee Amount: %s', l.estTotalFeeCreator);
 
-
-        
-            
         // set oracle price > predictedPrice => predictedTrue = true (yes wins)
         uint256 oraclePrice = 200;
         oracle.setPrice(oraclePrice);
@@ -355,6 +165,7 @@ function test_resolvePrediction_onlyWinnersBets() public {
         for (uint256 i = 0; i < usersNo.length; i++) {
             vm.prank(usersNo[i]);
             predicter.claim(creator);
+            assertEq(token.balanceOf(address(usersNo[i])), strikeAmount); 
         }
 
         // resolvedPrice should be set
@@ -366,7 +177,7 @@ function test_resolvePrediction_onlyWinnersBets() public {
         console2.log('beneficia balance after all claimes:  %s', token.balanceOf(address(feeBeneficiary)));
         console2.log('One Yes voter balance after claimes:  %s', token.balanceOf(usersNo[0]));
         assertEq(token.balanceOf(address(predicter)), 0); 
-    }
-
-    
+        assertEq(token.balanceOf(creator), 0); 
+        assertEq(token.balanceOf(predicter.FEE_PROTOCOL_BENEFICIARY()), 0); 
+    }   
 }
