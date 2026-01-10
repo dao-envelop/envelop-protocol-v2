707107c31922491788ff7bd46d5b44f6b96d33bb
Alex <alexamuek@gmail.com>
2026-01-10 17:18:10 +0800

    add tests for oracle

--- test/fork/EnvelopOracle_a_01.t.sol
+++ test/fork/EnvelopOracle_a_01.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.24;
 
 import "forge-std/Test.sol";
 import "../../src/utils/EnvelopOracle.sol";
+import "../../src/utils/Predicter.sol";
 
 contract EnvelopOracle_a_01 is Test {
     EnvelopOracle internal oracle;
@@ -10,6 +11,7 @@ contract EnvelopOracle_a_01 is Test {
     address feedRegistry = 0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf;
     uint256 maxStale = 36000000000;
     address usdt = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address dai = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address niftsy = 0x7728cd70b3dD86210e2bd321437F448231B81733;
     uint8 usdtDecimals = 8;
 
@@ -38,5 +40,27 @@ contract EnvelopOracle_a_01 is Test {
         assertGt(roundId, 0);
         assertGt(updatedAt, 0);
         assertEq(decimals, usdtDecimals);
-    }        
+    }      
+
+    function test_getPriceInUSDWithMeta_nonPricableToken() public {
+        vm.expectRevert("Feed not found");
+        oracle.getPriceInUSDWithMeta(niftsy);
+    }    
+
+    function test_getIndexPrice_success() public {
+        CompactAsset[] memory assets = new CompactAsset[](2);
+        assets[0] = CompactAsset({token: usdt, amount: 1});
+        assets[1] = CompactAsset({token: dai, amount: 1});
+        uint256 actualPrice = oracle.getIndexPrice(assets);
+        assertApproxEqAbs(actualPrice, 2 * 10**usdtDecimals, 10**(usdtDecimals - 2));
+    }
+
+    function test_getIndexPrice_nonPricableToken() public {
+        CompactAsset[] memory assets = new CompactAsset[](2);
+        assets[0] = CompactAsset({token: usdt, amount: 1});
+        assets[1] = CompactAsset({token: niftsy, amount: 1});
+        vm.expectRevert("Feed not found");
+        oracle.getIndexPrice(assets);
+    }
+     
 }
